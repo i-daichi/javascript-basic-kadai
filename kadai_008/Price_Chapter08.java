382a54f475bb8f6bec4d0684dea4a049c676ad7e
i-daichi <161053666+i-daichi@users.noreply.github.com>
2024-10-02 14:54:07 +0900

    Add files via upload

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -9,11 +9,11 @@ public class Price_Chapter08 {
 		case 10 -> 1000;
 		case 20 -> 2000;
 		case 30 -> 3000;
-		case 40 -> 4000;
-		case 50 -> 5000;
-		case 60 -> 6000;
-		case 70 -> 7000;
-		case 80 -> 8000;
+		case 40 -> 3000;
+		case 50 -> 4000;
+		case 60 -> 4000;
+		case 70 -> 4000;
+		case 80 -> 5000;
 		default -> 500;
 		
 		};
